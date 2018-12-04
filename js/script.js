bc1cecda04920d85d27af9ab70a4278f8fb854dd
Zeztron <pateltharsh@gmail.com>
2018-12-04 11:19:46 -0500

    Added the display featured car feature

--- js/script.js
+++ js/script.js
@@ -31,11 +31,11 @@ const CreateCars = (() => {
     function produceCars() {
         makeCar("mercedes", "german", "img/car-german-1.jpeg");
         makeCar("bmw", "german", "img/car-german-2.jpeg");
-        makeCar("bmw", "german", "img/car-german-3.jpeg");
-        makeCar("bmw", "german", "img/car-german-4.jpeg");
+        makeCar("bmw", "german", "img/car-german-3.jpeg", false);
+        makeCar("bmw", "german", "img/car-german-4.jpeg", false);
         makeCar("bmw", "german", "img/car-german-5.jpeg");
-        makeCar("chevy", "american", "img/car-american-1.jpeg", false);
-        makeCar("chevy", "american", "img/car-american-2.jpeg", false);
+        makeCar("chevy", "american", "img/car-american-1.jpeg");
+        makeCar("chevy", "american", "img/car-american-2.jpeg");
         makeCar("chevy", "american", "img/car-american-3.jpeg", false);
         makeCar("chevy", "american", "img/car-american-4.jpeg", false);
         makeCar("chevy", "american", "img/car-american-5.jpeg", false);
@@ -51,5 +51,39 @@ const CreateCars = (() => {
 
 })();
 
-console.log(CreateCars.cars);
-console.log(CreateCars.specialCars)
+// console.log(CreateCars.cars);
+// console.log(CreateCars.specialCars)
+
+const DisplaySpecialCars = ((CreateCars) => {
+    const specialCars = CreateCars.specialCars;
+    // console.log(specialCars);
+    const info = document.querySelector(".featured-info");
+    // Document loaded event
+    document.addEventListener("DOMContentLoaded", () => {
+        info.innerHTML = '';
+        let data = '';
+        specialCars.forEach(item => {
+            data += ` 
+                <div class="featured-item my-3 d-flex p-2 text-capitalize align-items-baseline flex-wrap">
+                    <span data-img="${item.img}" class="featured-icon mr-2">
+                        <i class="fas fa-car"></i>
+                    </span>
+                    <h5 class="font-weight-bold mx-1">${item.make}</h5>
+                    <h5 class="mx-1">${item.model}</h5>
+                </div>`;
+        })
+        info.innerHTML = data;
+    })
+
+    // Change image
+    info.addEventListener("click", (event) => {
+        // console.log(event.target.parentElement);
+        if(event.target.parentElement.classList.contains('featured-icon')) {
+            // console.log("hi")
+            const img = event.target.parentElement.dataset.img;
+            // console.log(img)
+            document.querySelector(".featured-photo").src = img;
+        }
+    })
+    
+})(CreateCars);
